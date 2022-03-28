b71d5a950bdcd4a255bcb5b91195d0c2ee397ad0
unknown <mglezos1@gmail.com>
2022-03-27 22:51:52 -0500

    initial commit

---
+++ src/components/AccountModal.tsx
@@ -0,0 +1,21 @@
+import {
+    Box,
+    Button,
+    Flex,
+    Link,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Text,
+  } from "@chakra-ui/react";
+  import { ExternalLinkIcon, CopyIcon } from "@chakra-ui/icons";
+  import { useEthers } from "@usedapp/core";
+  import Identicon from "./Identicon";
+  
+  export default function AccountModal() {
+      
+  }
